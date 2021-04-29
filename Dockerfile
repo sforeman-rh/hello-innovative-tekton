6f2c2b67a5ec671b99a817c0270c04d8d36e02fd
C.J. Nuland <cjnuland@gmail.com>
2021-04-28 21:59:35 -0400

    added a lot with the eventlistener

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM quay.io/app-sre/ubi8-nodejs-10:latest
 MAINTAINER Chris Nuland
-ADD ../src/main.js $HOME/main.js
-ADD ../src/package.json $HOME/package.json
+ADD ../main.js $HOME/main.js
+ADD ../package.json $HOME/package.json
 EXPOSE 8080
--- hello-chris/main.js
+++ hello-chris/main.js
@@ -9,4 +9,4 @@ var server = http.createServer(function (request, response) {
 
 //Start server
 server.listen(8080);
-console.log("Server listening on http://127.0.0.1:8080/");
+console.log("Hello Chris: Server listening on http://127.0.0.1:8080/");
