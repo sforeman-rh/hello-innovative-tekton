59898ff7473d8e5104b2b0f3e54a8b5778c50dc2
ROSA User <rosa@bastion.qwsqr.internal>
2023-10-05 19:17:40 +0000

    include requirements from index module

--- hello-innovative/main.py
+++ hello-innovative/main.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello, this is version 2!"
 
 if __name__ == "__main__":
+    print("hello world!")
     app.run(host='0.0.0.0')
