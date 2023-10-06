3d8c180576893cd20f5994975b6de4c824170b2d
ROSA User <rosa@bastion.qwsqr.internal>
2023-10-06 16:08:09 +0000

    fix bug with domain and add prompt

---
+++ tailwinds-prompt/src/main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Welcome to my app!"
+
+if __name__ == "__main__":
+    print("this is prompt!")
+    app.run(host='0.0.0.0')
